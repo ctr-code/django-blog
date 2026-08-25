69448c928c34c1b2f27fcc1936dcb109e4134114
ctr-code <codeinstitute@charlesreilly.com>
2026-08-25 14:15:08 +0100

    Add ordering and __str__ for Post and Comment models

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,12 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.title} | by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -31,3 +37,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
