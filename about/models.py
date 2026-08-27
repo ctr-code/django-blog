135207bd8c89336fed2361f316f4f000ab73ddc0
ctr-code <codeinstitute@charlesreilly.com>
2026-08-27 12:39:14 +0100

    Add a __str__ method to the About model

--- about/models.py
+++ about/models.py
@@ -5,3 +5,7 @@ class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+
+    # The about message is a singleton
+    def __str__(self):
+        return "About Page Content"
