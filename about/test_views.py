9c54a7946cda58234d559c2e2d1ed0ab7e0da737
ctr-code <codeinstitute@charlesreilly.com>
2026-08-29 14:52:32 +0100

    Add tests for POSTs

--- about/test_views.py
+++ about/test_views.py
@@ -1,9 +1,8 @@
 import datetime
-from django.contrib.auth.models import User
 from django.urls import reverse
 from django.test import TestCase
 from .forms import CollaborationForm
-from .models import About
+from .models import About, CollaborationRequest
 
 
 class TestAboutViews(TestCase):
@@ -22,3 +21,25 @@ class TestAboutViews(TestCase):
         self.assertIn(b"Sausages", response.content)
         self.assertIsInstance(
             response.context['collaboration_form'], CollaborationForm)
+
+    def test_successful_collaboration_request(self):
+        """Test for posting a collaboration request"""
+        post_data = {
+            'name': 'John Smith',
+            'email': 'john@smith.com',
+            'message': 'gizza job!',
+        }
+        response = self.client.post(reverse(
+            'about'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Your collaboration message has been received and will be seen',
+            response.content
+        )
+        # Check the request was created in the database
+        self.assertEqual(CollaborationRequest.objects.all().count(), 1)
+        request = CollaborationRequest.objects.first()
+        self.assertEqual(post_data['name'], request.name)
+        self.assertEqual(post_data['email'], request.email)
+        self.assertEqual(post_data['message'], request.message)
+        self.assertEqual(request.read, False)
